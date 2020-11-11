1047ed508667517fbb27dfd5a027237c872a6ae0
Raimund Haslehner <Raimund.Haslehner@students.fh-wels.at>
2020-11-11 16:27:36 +0100

    Correction in test_add_frac 3

--- test_add_frac.py
+++ test_add_frac.py
@@ -16,5 +16,5 @@ Dieses Skript testet die Funktion add_frac von add_frac.
 from add_frac import add_frac
 
 def test_add_frac():
-	assert (3,4,1,3) == Ergebnis:  13 / 12
+	assert (3,4,1,3) == print("Ergebnis:", 13, "/", 12)
 
