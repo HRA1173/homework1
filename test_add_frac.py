3c2eed8ee1ea3529d1642125ae2b052217a788a1
Raimund Haslehner <Raimund.Haslehner@students.fh-wels.at>
2020-11-11 16:02:32 +0100

    Add python test script

---
+++ test_add_frac.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  11 16:00:36 2020
+
+@author: User
+"""
+
+
+"""
+Dieses Skript testet die Funktion add_frac von add_frac.
+
+"""
+ 
+
+
+from add_frac import add_frac
+
+def test_add_frac():
+	assert (3,4,1,3)== print("Ergebnis:", 3, "/", 12)
+
